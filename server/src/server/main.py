affe1e9b715d095a7ac739518a79432a14c7f83a
svidovich <samuel.vidovich@gmail.com>
2025-04-05 23:45:53 -0400

    Several updates... - Change the default small model - Set up pipelining - Ensure that PIL images are saved to buffers and returned correctly - Add generation options that ( hopefully ) align with machine   good-itude, and pipe them through the generation functions   These are operated based on environment vars so far.
    It generated an image -- Yay\! We need to add more models to the list.
    It might be beneficial to add them as defaults to the default options
    for the different machine types. We'll see.
    
    Also, We should bottom out at 512x512 for SD. It was apparently trained
    on that size, and below that gives you eldritch abominations.

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -1,10 +1,24 @@
-import json
+"""
+The main entrypoint for the stable-diffusion-environment server.
+Use run.sh or something. Uvicorn works, too.
+"""
+
+# pylint: disable = global-statement,protected-access,broad-exception-caught
+# I use the global statement to help define singletons.
+# I use protected access in this module to deal with the current worker count in a given
+# ThreadPool instance and its overall worker count, which I don't think are exposed in
+# a more transparent way.
+# I catch broad exceptions to ensure the uptime of the server. Exceptions are reported
+# back through the API and logging.
+
+import io
 import os
 from _thread import LockType
 from base64 import b64decode, b64encode
 from collections.abc import Callable
 from contextlib import asynccontextmanager
 from datetime import datetime
+from enum import StrEnum, auto
 from http import HTTPStatus
 from json import JSONDecodeError
 from multiprocessing.pool import ThreadPool
@@ -29,7 +43,97 @@ from pydantic import (
 from typing_extensions import Annotated
 
 MAX_THREADS = 1
-MODEL_SMALL = "stabilityai/stable-diffusion-2-1-unclip-small"
+MODEL_SMALL = "stabilityai/stable-diffusion-2-1"
+
+
+class MachineSize(StrEnum):
+    """
+    Counter strike Beefy Computer?
+    Or StrongBad's Lappy?
+    """
+
+    BIG = auto()
+    MEDIUM = auto()
+    LITTLE = auto()
+    BABY = auto()
+
+
+DEFAULT_MACHINE_SIZE = MachineSize.LITTLE
+
+
+class GenerationOptions(BaseModel):
+    """
+    Options for using SD pipelines depending on our resource constraints.
+    """
+
+    size: MachineSize
+    enable_attention_slicing: bool
+    enable_sequential_cpu_offload: bool
+    image_height: int  # TODO needs to be a multiple of 64. Write a validator.
+    image_width: int  # TODO needs to be a multiple of 64. Write a validator.
+
+
+GENERATION_DEFAULTS_LIST = [
+    GenerationOptions(
+        size=MachineSize.BIG,
+        enable_attention_slicing=False,
+        enable_sequential_cpu_offload=False,
+        image_height=1024,
+        image_width=1024,
+    ),
+    GenerationOptions(
+        size=MachineSize.MEDIUM,
+        enable_attention_slicing=True,
+        enable_sequential_cpu_offload=False,
+        image_height=512,
+        image_width=512,
+    ),
+    GenerationOptions(
+        size=MachineSize.LITTLE,
+        enable_attention_slicing=True,
+        enable_sequential_cpu_offload=False,
+        image_height=384,
+        image_width=384,
+    ),
+    GenerationOptions(
+        size=MachineSize.BABY,
+        enable_attention_slicing=True,
+        enable_sequential_cpu_offload=True,
+        image_height=256,
+        image_width=256,
+    ),
+]
+
+GENERATION_DEFAULTS = {entry.size: entry for entry in GENERATION_DEFAULTS_LIST}
+
+
+def generation_defaults(machine_size: MachineSize) -> GenerationOptions:
+    """
+    Get the default image generation options for the input machine size.
+    """
+    return GENERATION_DEFAULTS[machine_size]
+
+
+def generation_options_from_environment() -> GenerationOptions:
+    """
+    Attempt to get our generation options from the environment. Failing that, get sensible
+    defaults.
+    """
+    if env_machine_size := os.environ.get("MACHINE_SIZE"):
+        try:
+            return generation_defaults(machine_size=MachineSize(env_machine_size))
+        except ValueError:
+            logger.error(
+                f"Invalid MACHINE_SIZE `{env_machine_size}`; defaulting to "
+                f"`{DEFAULT_MACHINE_SIZE}`."
+            )
+            return generation_defaults(machine_size=DEFAULT_MACHINE_SIZE)
+    else:
+        logger.info(
+            f"Didn't find MACHINE_SIZE in the environment; defaulting to "
+            f"`{DEFAULT_MACHINE_SIZE}`."
+        )
+        return generation_defaults(machine_size=DEFAULT_MACHINE_SIZE)
 
 
 def get_default_disk_flush_directory(ensure_directory: bool = True) -> Path:
@@ -72,7 +176,7 @@ def model_cache_dir(model_name: str, ensure_directory: bool = True) -> Path:
 pipeline: DiffusionPipeline | None = None
 
 
-def load_pipeline(model_name: str) -> DiffusionPipeline:
+def load_pipeline(model_name: str, options: GenerationOptions) -> DiffusionPipeline:
     """
     Get a singleton DiffusionPipeline.
     """
@@ -81,11 +185,25 @@ def load_pipeline(model_name: str) -> DiffusionPipeline:
         pipeline = DiffusionPipeline.from_pretrained(
             model_name,
             cache_dir=model_cache_dir(model_name=model_name),
-            torch_dtype=torch.float16,
         )
         assert pipeline, "Couldn't get the pipeline! Ahh!"
-        # looks like typing on this library sucks
-        pipeline.to("cuda")  # type: ignore[attr-defined]
+        # The typing for this library is very bad. They could use mypy in their system.
+        # It's pooching my code. booo.
+        if options.enable_attention_slicing:
+            logger.info("NOTE: Enabling attention slicing due to settings.")
+            # This helps us save some memory on a constrained system
+            pipeline.enable_attention_slicing()  # type: ignore[attr-defined]
+        if options.enable_sequential_cpu_offload:
+            logger.info("NOTE: Enabling sequential CPU offload due to settings.")
+            # Generally we do this when we're super short on resources. Not for the
+            # faint of heart
+            pipeline.enable_sequential_cpu_offload()  # type: ignore[attr-defined]
+            # Note that we don't pipeline _to_ anything: we defer that command to the
+            # library, which will handle it on its own. We can get goofy errors if we
+            # attempt to do our own piping.
+        else:
+            # looks like typing on this library sucks
+            pipeline.to("cuda")  # type: ignore[attr-defined]
     return pipeline
 
 
@@ -121,7 +239,8 @@ def async_pool_execution(pool: ThreadPool, function: Callable, **kwargs) -> None
 
 def load_base64(value: Any) -> bytes | None:
     """
-    Try to load base64 from whatever you throw its way. If it's already bytes, just gives back bytes.
+    Try to load base64 from whatever you throw its way. If it's already bytes, just
+    gives back bytes.
     Raises ValueError if it's not bytes or a string.
     Raises something interesting if it's an invalid base64 string.
     """
@@ -136,8 +255,8 @@ def load_base64(value: Any) -> bytes | None:
 
 def load_datetime_from_isoformat(value: Any) -> datetime:
     """
-    Try to load a datetime from whatever you throw its way. It's expecting an ISO formatted date string.
-    If it's already a datetime, though, it'll hand it back to you.
+    Try to load a datetime from whatever you throw its way. It's expecting an ISO
+    formatted date string. If it's already a datetime, it'll hand it back to you.
     Raises ValueError if you pass it garbage.
     """
     if isinstance(value, datetime):
@@ -164,7 +283,8 @@ class GenerationResult(BaseModel):
         """
         Add an error to the list of errors.
         """
-        self.errors.append(error)
+        # Pylint can't see as deeploy as mypy.
+        self.errors.append(error)  # pylint: disable=no-member
 
     @field_serializer("created_time")
     def serialize_created_time(self, created_time: datetime) -> str:
@@ -226,7 +346,7 @@ class GenerationOutputs(BaseModel):
         # Oh please kill me.
         with self.lock:  # type: ignore[attr-defined] # pylint: disable=no-member
             self.computations[result.uuid] = result
-
+        self.flush_to_disk()
         return self
 
     def flush_to_disk(self, path: Path | None = None) -> None:
@@ -281,7 +401,7 @@ class GenerationOutputs(BaseModel):
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app_: FastAPI):
     """
     A lifespan for the app, accessible through request.state. With it, you get...
     - request.state.pool: ThreadPool - one-man-army ThreadPool for generating images
@@ -315,21 +435,30 @@ class ImageGenerationResponse(BaseModel):
     generation_uuid: str
 
 
-def prompt_stable_diffusion(uuid: UUID, prompt: str) -> GenerationResult:
+def prompt_stable_diffusion(
+    uuid: UUID, prompt: str, options: GenerationOptions
+) -> GenerationResult:
     """
     Give the prompt to stable diffusion, bringing back the generated image.
     """
     logger.info(f"Beginning computation of prompt with ID `{uuid}`.")
     result = GenerationResult(uuid=uuid, prompt=prompt, data=None, errors=[])
     try:
-        pipeline = load_pipeline(model_name=MODEL_SMALL)
+        sd_pipeline = load_pipeline(model_name=MODEL_SMALL, options=options)
         # According to the comments in pipeline_utils.py from diffusers.pipelines,
         # here images is a List[PIL.Image.Image]. Then, ideally generated_image results as an
         # image from PIL, and we can just convert it to bytes.
         # Mypy thinks it's not callable. But, the resultant DiffusionPipeline from load_pipeline
         # _should_ be ... maybe.
-        generated_image: Image = pipeline(prompt).images[0]  # type: ignore[operator]
-        result.data = generated_image.tobytes()
+        with torch.inference_mode():  # Disables some training-time settings to save resources.
+            generated_image: Image = sd_pipeline(  # type: ignore[operator]
+                prompt, height=options.image_height, width=options.image_width
+            ).images[0]
+        # A buffer where we can dump our image. No, you can't just say tobytes().
+        image_buffer = io.BytesIO()
+        generated_image.save(image_buffer, format="PNG")
+        result.data = image_buffer.getvalue()
+
         logger.info(f"Completed generation for prompt with ID `{uuid}`!")
     except Exception as exc:
         logger.exception(
@@ -347,8 +476,9 @@ def generate_image(
     GenerationOutputs under the provided UUID.
     """
     logger.info(f"Received a prompt! `{prompt}`")
+    options = generation_options_from_environment()
     generation_outputs.add_generation_result(
-        result=prompt_stable_diffusion(uuid=uuid, prompt=prompt)
+        result=prompt_stable_diffusion(uuid=uuid, prompt=prompt, options=options)
     )
     logger.info(f"Result with UUID `{uuid}` added!")
 
